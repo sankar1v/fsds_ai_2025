201c43fc143513191d192993ce88daacb9b86eac
sankar1v <sankar1v@users.noreply.github.com>
2025-05-14 19:44:34 +0800

    Add files via upload

---
+++ 20250511/Task-1/T2- Variables.py
@@ -0,0 +1,40 @@
+
+# Variables in Python
+
+first_name = 'SANKAR'
+last_name = 'VINNAKOTA'
+country = 'SINGAPORE'
+city = 'TAMPINES'
+age = 42
+is_married = True
+skills = ['Spring Boot', 'Neo4j', 'Oracle', 'Java', 'Python']
+person_info = {
+    'firstname':'Asabeneh', 
+    'lastname':'Yetayeh', 
+    'country':'Finland',
+    'city':'Helsinki'
+    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'sankar', 'vinnakota', 'Singapore', 42, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
